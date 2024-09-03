7c88a5575d8cf4bbeaad1c6b05ba33fd0a022645
Federico Vagnini <vagnini@it.ibm.com>
2024-09-03 23:57:14 +0200

    fixed oc path in worker_csr.ksh

--- include/worker_csr.sh
+++ include/worker_csr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-../oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs -r ../oc adm certificate approve 
+../ocp/oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs -r ../ocp/oc adm certificate approve 

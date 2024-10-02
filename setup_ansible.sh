0d579d999bef44b49f30ed91784b120a922fdf82
Federico Vagnini <vagnini@it.ibm.com>
2024-10-02 18:28:33 +0200

    Update setup_ansible.sh

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,4 +1,3 @@
 sudo dnf -y install ansible-core libxml2-devel libxslt-devel gcc python3-devel python3-netaddr
 ansible-galaxy collection install ibm.power_hmc ansible.utils ansible.posix community.general
 sudo pip install lxml
-chmod +x butane
